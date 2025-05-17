1333fadf39325d9bb5ea8f8deffbd70f3050cb45
girmayevangadi <girmayabrha@gmail.com>
2025-05-17 14:46:53 -0700

    final

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Netflex-clone-2025/', // 👈 THIS IS CRITICAL
+
   plugins: [react()],
 })
+// vite.config.js
